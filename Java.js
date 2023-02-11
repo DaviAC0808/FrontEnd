dcc352f51f9f559fd07d583ceebedbf0719838c8
Davi Aguiar <daviaguiarcardoso@gmail.com>
2023-02-11 10:00:54 -0300

    iniciando var's

---
+++ Java.js
@@ -0,0 +1,18 @@
+window.alert('Bem vindo à area de JavaScript');
+
+window.confirm('Confirma a entrada no site?');
+
+var nome = window.prompt('insira seu nome:');
+window.alert('Seu nome é: ' + nome);
+
+window.confirm('Confirme que leu isso');
+
+/*Somando números*/
+
+/*conversão dos dados*/
+var n1 = Number.parseInt(window.prompt('Digite o primeiro valor:'));/*int,float*/
+var n2 = Number.parseInt(window.prompt('Digite o segundo valor:'));
+/*Manipulação dos dados*/
+var s = n1 + n2
+/*print*/
+window.alert('A soma entre eles é: ' + s);
